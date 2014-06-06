e9e246567e58d6dde890931836bac861ab817fa1
Brian Lennon <blennon@sofra>
2014-06-06 21:35:26 +0300

    update-readme.sh: Extend functionality
    - Move some instructions into a function
    - Create and preview file, with some dialogue, rather than just stdout

--- update-readme.sh
+++ update-readme.sh
@@ -22,10 +22,32 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readme="${1}"/README.md
 preview_app="Marked.app"
 
-head -4 "${1}"/README.md
-echo ""
-echo ""
-"${script_dir}"/catalog-files.sh "${1}"
-echo ""
-echo ""
-"${script_dir}"/changelog.sh "${1}"
+function build_readme {
+    readme_header="${2}"
+    echo "${readme_header}"
+    echo ""
+    "${script_dir}"/catalog-files.sh "${1}"
+    echo ""
+    echo ""
+    "${script_dir}"/changelog.sh "${1}"
+}
+
+readme_header=$(head -4 "${readme}")
+build_readme "${1}" "${readme_header}"
+echo -n "OK? (y/n) "
+read answer
+if [ $(echo "${answer}") = 'y' ]; then
+    rm -i "${readme}"
+    build_readme "${1}" "${readme_header}" > "${readme}"
+    cat "${readme}"
+    echo -n "Preview ${readme}? (y/n) "
+    read answer
+    if [ $(echo "${answer}") = 'y' ]; then
+        open -a "${preview_app}" "${readme}" 
+    else
+        exit 0
+    fi
+    exit 0
+else
+    exit 0
+fi
