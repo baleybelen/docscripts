442a568913204018a24b747d99510231dd7cfb07
Brian Lennon <blennon@bitfragment.net>
2014-07-12 13:29:49 +0300

    changelog.sh: Rewrite using 'test' instead of 'if...then'
    * Using 'test' and control operators ('&&' and '||') instead of
      if...then is more compact and not necessarily less readable
    
    * Previously, tested for at least one argument; now test for
      exactly one argument and tests to see that it exists as a
      directory.
    
    * Add correct exit codes.
    
    * Reorganize, moving variable assignments to start

--- changelog.sh
+++ changelog.sh
@@ -5,6 +5,12 @@ set -o errexit
 # set -o nounset # Interferes with use of unbound variables
 # set -o xtrace
 
+my_directory="${1}"
+git="/usr/bin/git"
+
+# Commit messages to exclude:
+message_excludes="Initial commit|.gitignore|README"
+
 function display_usage {
 cat <<EOF
 Usage: $(basename "${0}") <directory>
@@ -12,26 +18,17 @@ Usage: $(basename "${0}") <directory>
 EOF
     }
 
-if [ $# -lt 1 ]; then
-    display_usage
-    exit 2
-fi
-
-my_directory="${1}"
-git="/usr/bin/git"
-
-# Commit messages to exclude:
-message_excludes="Initial commit|.gitignore|README"
-
 function print_underscored_string {
     # String is $1, underscore character is $2
     size="${#1}"
     char="${2}"
     echo "${1}"
     printf "%0.s${2}" $(seq 1 "${size}")
-}
+    }
 
-print_underscored_string "Changelog" "-"
+test $# -eq 1 -a -d "${my_directory}" &&
+
+print_underscored_string "Changelog" "-" &&
 
 # If there are no annotated tags, get commit messages from log
 if [[ -z $("${git}" -C "${my_directory}" tag -l) ]]; then
@@ -51,4 +48,6 @@ else
         GIT_PAGER=cat "${git}" -C "${my_directory}" tag --list -n99 \
             "${tag}" | sed -e 's/^v[0-9a-zA-Z.-_]* *//' -e 's/^[ \t]*//'
     done
-fi
+fi &&
+
+exit 0 || display_usage && exit 1
